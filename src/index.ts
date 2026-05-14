95511aac2bf17c0141e3903ba8424f2b46d29bb6
Favo Yang <125390+favoyang@users.noreply.github.com>
2026-05-15 04:14:24 +0800

    Fix tarball route middleware on Node 20

--- src/index.ts
+++ src/index.ts
@@ -25,11 +25,7 @@ export default class VerdaccioMiddlewarePlugin implements IPluginMiddleware<Cust
     _storage: IStorageManager<CustomConfig>,
   ): void {
     // Define the middleware to insert before the download endpoint
-    app.use('/:package/-/:filename', async (req, res, next) => {
-      if (req.method === 'HEAD') {
-        next();
-        return;
-      }
+    app.get('/:package/-/:filename', async (req, res, next) => {
       const self = this;
       const packageName = req.params.package;
       const fileName = req.params.filename;
